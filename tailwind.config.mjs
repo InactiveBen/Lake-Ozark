22b656353e4bd2b17614b9debc2bcd5ac99c20e9
InactiveBen <zack@fishbowl.cloud>
2024-11-01 07:43:36 -0500

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+      colors: {
+        brand: {
+          DEFAULT: '#9e1e3e',
+          light: '#b84660',
+          dark: '#841834',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
