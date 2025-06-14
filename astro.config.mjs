66a71181688da60d4d9a99b1100056132835cf5e
InactiveBen <ben@pixelpulse.games>
2025-06-14 06:39:39 -0500

    FIX: Sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,24 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-import sentry from '@sentry/astro';
-
 export default defineConfig({
   integrations: [
     tailwind(), 
-    react(), 
-    sentry({
-      dsn: "https://d4be77c82b4c10fc61de7d57705ddc7b@o4509496802738176.ingest.us.sentry.io/4509496803786752",
-      // Disable automatic initialization - we'll handle it with cookie consent
-      autoInstrument: false,
-      // Setting this option to true will send default PII data to Sentry.
-      // For example, automatic IP address collection on events
-      sendDefaultPii: true,
-      sourceMapsUploadOptions: {
-        project: "javascript-astro",
-        authToken: process.env.SENTRY_AUTH_TOKEN,
-      },
-    })
+    react()
   ],
   markdown: {
     shikiConfig: {
